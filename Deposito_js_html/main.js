b38ac209c7c3af427df053dddc5f9fc78a4756e1
williamsdamasceno <133906052+williamsdamasceno@users.noreply.github.com>
2023-08-11 07:36:05 -0300

    Add files via upload

---
+++ Deposito_js_html/main.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+
+function validaNome(nomeCompleto){
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
+}
+form.addEventListener('submit', function(e){
+    let formEValido = false ;
+    e.preventDefault();
+
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta <b>${numeroContaBeneficiario.value}</b>` ;
+    
+    formEValido = validaNome(nomeBeneficiario.value)
+    if (formEValido) {
+        const containerMensagemSucesso =  document.querySelector(`.sucess-message`);
+        containerMensagemSucesso.innerHTML = mensagemSucesso ;
+        containerMensagemSucesso.style.display = 'block';
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+    } else {
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message'); style.display = 'block';
+    } 
+})
+nomeBeneficiario.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    formEValido = validaNome(e.target.value);
+
+    if (formEValido) {
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message'); style.display = 'block';
+    } else {
+        nomeBeneficiario.style.border = 'none';
+        document.querySelector('.error-message'); style.display = 'none';
+
+    }
+});
+
